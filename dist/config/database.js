3790c87cc29d3c4bf1958ca934c15ab798a7ca65
Marcelo Penha <niqcelo@gmail.com>
2021-05-30 13:46:42 -0300

    dist folder

---
+++ dist/config/database.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pg_1 = require("pg");
+const dotenv = require('dotenv');
+dotenv.config();
+const pool = new pg_1.Pool({
+    connectionString: process.env.DATABASE_URL
+});
+pool.on('connect', () => console.log("Conectado no Bando !!"));
+const query = {
+    query: (text, params) => pool.query(text, params)
+};
+exports.default = query;
