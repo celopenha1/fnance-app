d5ad839cf01852a5ab91a74b48db49f4b3ca0f78
Marcelo Penha <niqcelo@gmail.com>
2021-05-30 13:54:54 -0300

    CHANGE PORT

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,6 @@
 "use strict";
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+  return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
@@ -16,12 +16,13 @@ app.use(express_1.default.urlencoded({ extended: true }));
 app.use(express_1.default.json());
 app.use(express_1.default.json({ type: 'application/vnd.api+json' }));
 app.use(cors_1.default({
-    origin: "*"
+  origin: "*"
 }));
 // use routes
 app.use(index_1.default);
 app.use(transaction_routes_1.default);
 // app.use(index);
 // app.use('/api/', productRoute);
+const port = 3001 | process.env.PORT;
 app.listen(3001, () => console.log('caraleo'));
 exports.default = app;
