775f7b41b522dd144684dbf24d9fc9dd3bb90c43
Marcelo Filho <niqcelo@gmail.com>
2021-09-08 20:42:15 -0300

    trocando url de conexao p/local

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,9 +1,11 @@
 import { Pool } from 'pg';
+
+
 const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_LOCAL
 });
 
 pool.on('connect', () => console.log("Conectado no Bando !!"));
@@ -11,5 +13,13 @@ pool.on('connect', () => console.log("Conectado no Bando !!"));
 const query = {
   query: (text: string, params: Array<String>) => pool.query(text, params)
 }
+
+//NEWWW
+
+
+
+
+
+
 
 export default query;
